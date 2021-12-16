ec11de08323ebf80f525edf32a5fdaf41d073fe6
Starting <Starting@DESKTOP-D1ODSDE>
2021-12-16 21:19:50 +0330

    Add project files.

---
+++ Controllers/ClothController.cs
@@ -0,0 +1,15 @@
+﻿using MyFirstApp.Models;
+
+namespace MyFirstApp.Controllers
+{
+    public class ClothController
+    {
+       public List<ClotheViewModel> clothes = new List<ClotheViewModel>();
+        public AddItem(int id,int number,string model,string size,string category)
+        {
+            var newclothe = new ClotheViewModel( id, number, model, size, category );
+            clothes.Add(newclothe);
+            return;
+        }
+    }
+}
---
+++ Models/ClotheViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MyFirstApp.Models
+{
+    public class ClotheViewModel
+    {
+        public int id { get; set; }
+        public int number { get; set; }
+        public string model { get; set; }
+        public string size { get; set; }
+        public string category { get; set; }
+
+        public ClotheViewModel(int id, int number, string model, string size, string category)
+        {
+            this.id = id;
+            this.number = number;
+            this.model = model;
+            this.size = size;
+            this.category = category;
+        }
+
+    }
+}
